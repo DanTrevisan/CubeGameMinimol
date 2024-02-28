6929650b913950db4d25c714273d56d1ef76d7f3
Daniel Trevisan <daniboy_barca@hotmail.com>
2024-02-28 01:23:44 -0300

    First commit - gitIgnore + base project

---
+++ ProjetoMinimol/CubeJump/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    STATE_PAUSE,
+    STATE_PLAYING,
+    STATE_VICTORY,
+    STATE_DEFEAT
+}
+public static class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
